471a131d79ad018d8d9ccd216ed7a292bf4045b6
Nikolas Andryuschenko <nikolaigeorgie@gmail.com>
2025-07-02 15:27:02 -0700

    env playwright

--- components/features3.tsx
+++ components/features3.tsx
@@ -440,7 +440,7 @@ export default function Features3() {
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           <CustomerServiceSection />
           <LoyaltySection />
-          <FeaturedDishesSection />
+          <RestaurantHoursSection />
         </div>
       </div>
     </section>
